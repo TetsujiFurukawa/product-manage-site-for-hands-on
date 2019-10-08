3a5e60030247f6f6d14c452cbec32472f3726e5b
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2019-10-09 00:02:51 +0900

    🚧Edited xsrf intercepter.

--- src/app/intercepter/http-xsrf-interceptor.ts
+++ src/app/intercepter/http-xsrf-interceptor.ts
@@ -13,14 +13,18 @@ export class HttpXsrfInterceptor implements HttpInterceptor {
     let requestMethod: string = req.method;
     requestMethod = requestMethod.toLowerCase();
 
-    if (requestMethod && (requestMethod === 'post' || requestMethod === 'delete' || requestMethod === 'put')) {
+    if (requestMethod !== null && (requestMethod === 'post' || requestMethod === 'delete' || requestMethod === 'put')) {
+
+      // Puts xsrf token.
       const headerName = 'X-XSRF-TOKEN';
       const token = this.tokenExtractor.getToken() as string;
       if (token !== null && !req.headers.has(headerName)) {
         req = req.clone({ headers: req.headers.set(headerName, token) });
       }
+
     }
 
     return next.handle(req);
+
   }
 }

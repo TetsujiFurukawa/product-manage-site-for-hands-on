71bfbb0149fbbaf9ca5e476269073040f5bb503a
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2019-12-15 21:51:13 +0900

    🚧Adding unit test cases.

--- src/app/intercepter/http-xsrf-interceptor.spec.ts
+++ src/app/intercepter/http-xsrf-interceptor.spec.ts
@@ -12,23 +12,15 @@ import { User } from '../entity/user';
 import { ProductService } from '../service/product.service';
 import { HttpXsrfInterceptor } from './http-xsrf-interceptor';
 
-// xdescribe('HttpXsrfInterceptor', () => {
-//   it('should create an instance', () => {
-//     expect(new HttpXsrfInterceptor()).toBeTruthy();
-//   });
-// });
 describe('HttpXsrfInterceptor', () => {
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
-  // let intercepter: HttpXsrfInterceptor;
-  let productService: ProductService;
   let tokenExtractor: { getToken: jasmine.Spy };
 
   beforeEach(() => {
     tokenExtractor = jasmine.createSpyObj('HttpXsrfTokenExtractor', ['getToken']);
 
     TestBed.configureTestingModule({
-      // schemas: [NO_ERRORS_SCHEMA],
       imports: [HttpClientTestingModule],
       providers: [
         {
@@ -37,73 +29,29 @@ describe('HttpXsrfInterceptor', () => {
           multi: true
         },
         HttpXsrfTokenExtractor,
-        ProductService,
         { provide: HttpXsrfTokenExtractor, useValue: tokenExtractor }
       ]
     });
     httpTestingController = TestBed.get(HttpTestingController);
-    // intercepter = TestBed.get(HttpXsrfInterceptor);
-    productService = TestBed.get(ProductService);
-    // tokenExtractor = TestBed.get(HttpXsrfTokenExtractor);
     httpClient = TestBed.get(HttpClient);
   });
-
-  // afterEach(() => {
-  //   // After every test, assert that there are no more pending requests.
-  //   httpTestingController.verify();
-  // });
-
-  // describe('#constractor', () => {
-  //   it('should be created', () => {
-  //     expect(intercepter).toBeTruthy();
-  //   });
-  // });
 
   describe('#intercept', () => {
     const headerName = 'X-XSRF-TOKEN';
     const expectedToken = 'token';
 
-    it('should intercept get method', async(() => {
+    it('should intercept get method', () => {
       tokenExtractor.getToken.and.returnValue(expectedToken);
       httpClient.get('/test').subscribe(res => expect(res).toBeTruthy());
       const req = httpTestingController.expectOne({ method: 'GET' });
       expect(req.request.headers.get(headerName)).toEqual(expectedToken);
-      req.flush({});
-    }));
+    });
 
-    it('should intercept post method', async(() => {
+    it('should intercept post method', () => {
       tokenExtractor.getToken.and.returnValue(expectedToken);
       httpClient.post('/test', null).subscribe(res => expect(res).toBeTruthy());
       const req = httpTestingController.expectOne({ method: 'POST' });
       expect(req.request.headers.get(headerName)).toEqual(expectedToken);
-      req.flush({});
-
-      // httpClient.get('/test').subscribe(res => expect(res).toBeTruthy());
-      // const req = httpTestingController.expectOne({ method: 'GET' });
-      // expect(req.request.headers.get('X-XSRF-TOKEN')).toEqual('aaa');
-      // req.flush({
-      //   hello: 'world'
-      // });
-    }));
+    });
   });
-
-  // describe('#intercept', () => {
-  //   intercepter.intercept(new HttpRequest('HEAD', '/test'), null).subscribe();
-  //   const req = backend.expectOne('/test');
-  //   expect(req.request.headers.has('X-XSRF-TOKEN')).toEqual(false);
-  //   req.flush({});
-
-  //   const headerName = 'X-XSRF-TOKEN';
-
-  //   it('should intercept post method', () => {
-  //     const webApiUrl = UrlConst.PATH_API_FOLDER + ApiConst.PATH_PRODUCT;
-  //     const productDto: ProductDto = new ProductDto();
-  //     // spyOn(tokenExtractor, 'getToken').and.returnValue('token');
-  //     productService.updateProduct(productDto).subscribe(response => {
-  //       expect(response).toBeNull();
-  //       const httpRequest = httpTestingController.expectOne(webApiUrl);
-  //       expect(httpRequest.request.headers.has(headerName)).toEqual(true);
-  //     });
-  //   });
-  // });
 });

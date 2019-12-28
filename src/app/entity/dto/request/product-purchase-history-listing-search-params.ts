af09557681ddf4893bf4bab593569f7522de3bfe
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2019-12-29 03:21:38 +0900

    🍉Removed

--- src/app/entity/dto/request/product-purchase-history-listing-search-params.ts
+++
@@ -1,9 +0,0 @@
-import { BaseSearchParams } from './base-search-params';
-
-export class ProductPurchaseHistoryListingSearchParams extends BaseSearchParams {
-  productPurchaseName: string;
-  productPurchaseDateFrom: string;
-  productPurchaseDateTo: string;
-  productName: string;
-  productCode: string;
-}

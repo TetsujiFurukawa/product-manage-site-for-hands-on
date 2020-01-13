816d51e57d985116ff64fe7c3518b640a8a61f06
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2020-01-14 00:36:57 +0900

    ✨Added order date search criteria to purchase listing page test object.

--- e2e/src/purchase-history-listing-page.po.ts
+++ e2e/src/purchase-history-listing-page.po.ts
@@ -1,10 +1,11 @@
 import { by, element, ElementArrayFinder } from 'protractor';
 
+const SELECTOR_FOR_MAT_DATE_PICKER = ' > form > mat-form-field > div > div.mat-form-field-flex > div.mat-form-field-infix > input';
 export class PurchaseHistoryListingPage {
   private TEST_IDS = {
     PRODUCT_PURCHASE_NAME: 'product-purchase-name',
-    PRODUCT_PURCHASE_DATE_FROM: 'product-purchase-date-from',
-    PRODUCT_PURCHASE_DATE_TO: 'product-purchase-date-to',
+    PRODUCT_PURCHASE_DATE_FROM: '#product-purchase-date-from' + SELECTOR_FOR_MAT_DATE_PICKER,
+    PRODUCT_PURCHASE_DATE_TO: '#product-purchase-date-to' + SELECTOR_FOR_MAT_DATE_PICKER,
     PRODUCT_NAME: 'product-name',
     PRODUCT_CODE: 'product-code',
     SEARCH_RESULT: '.results > tbody > tr',
@@ -51,8 +52,8 @@ export class PurchaseHistoryListingPage {
    */
   setupSearchCriteria(productPurchaseName: string, productPurchaseDateFrom: string, productPurchaseDateTo: string, productName: string, productCode: string): PurchaseHistoryListingPage {
     element(by.id(this.TEST_IDS.PRODUCT_PURCHASE_NAME)).sendKeys(productPurchaseName);
-    element(by.id(this.TEST_IDS.PRODUCT_PURCHASE_DATE_FROM)).sendKeys(productPurchaseDateFrom);
-    element(by.id(this.TEST_IDS.PRODUCT_PURCHASE_DATE_TO)).sendKeys(productPurchaseDateTo);
+    element(by.css(this.TEST_IDS.PRODUCT_PURCHASE_DATE_FROM)).sendKeys(productPurchaseDateFrom);
+    element(by.css(this.TEST_IDS.PRODUCT_PURCHASE_DATE_TO)).sendKeys(productPurchaseDateTo);
     element(by.id(this.TEST_IDS.PRODUCT_NAME)).sendKeys(productName);
     element(by.id(this.TEST_IDS.PRODUCT_CODE)).sendKeys(productCode);
     return this;

fe3214273fc1f87b82e6f24d672ae36a4255a54a
FurukawaTetsuji <tetsufuru1968@yahoo.co.jp>
2020-04-29 15:17:42 +0900

    😎Reviewd sign-in page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { RoutingService } from './core/services/routing.service';
+import { UrlConst } from './pages/constants/url-const';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,21 @@ import { RoutingService } from './core/services/routing.service';
 export class AppComponent {
   title = 'product-manage-site-for-hands-on';
 
-  constructor(public routingService: RoutingService) {}
+  constructor(private routingService: RoutingService) {}
+
+  /**
+   * Determines whether sign in page is displayed
+   * @returns true if sign in page
+   */
+  public isSignInPage(): boolean {
+    console.log('AppComponent #isSignInPage() this.routingService.router.url' + this.routingService.router.url);
+
+    if (UrlConst.SLASH === this.routingService.router.url) {
+      return true;
+    }
+    if (UrlConst.SLASH + UrlConst.PATH_SIGN_IN === this.routingService.router.url) {
+      return true;
+    }
+    return false;
+  }
 }

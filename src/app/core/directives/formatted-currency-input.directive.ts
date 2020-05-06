e82fe050f6e2590cc50cd1fa86ccd0c5b7d77c7d
FurukawaTetsuji <tetsufuru1968@yahoo.co.jp>
2020-05-06 17:23:02 +0900

    🚧Reviewing directives.

--- src/app/core/directives/formatted-currency-input.directive.ts
+++ src/app/core/directives/formatted-currency-input.directive.ts
@@ -1,6 +1,10 @@
 import { Directive, ElementRef, HostListener, OnInit } from '@angular/core';
+import { NgControl } from '@angular/forms';
 
 import { FormattedCurrencyPipe } from '../pipes/formatted-currency.pipe';
+
+const LOCALE = 'locale';
+const CURRENCY = 'currency';
 
 @Directive({
   selector: '[appFormattedCurrencyInput]'
@@ -8,21 +12,63 @@ import { FormattedCurrencyPipe } from '../pipes/formatted-currency.pipe';
 export class FormattedCurrencyInputDirective implements OnInit {
   private element: HTMLInputElement;
 
-  constructor(private elementRef: ElementRef, private formattedCurrencyPipe: FormattedCurrencyPipe) {}
+  constructor(
+    private elementRef: ElementRef,
+    private formattedCurrencyPipe: FormattedCurrencyPipe,
+    private ngControl: NgControl
+  ) {}
 
   ngOnInit() {
     this.element = this.elementRef.nativeElement;
 
-    this.element.value = this.formattedCurrencyPipe.transform(this.element.value, this.element.getAttribute('locale'), this.element.getAttribute('currency'));
+    this.element.value = this.formattedCurrencyPipe.transform(
+      this.element.value,
+      this.element.getAttribute(LOCALE),
+      this.element.getAttribute(CURRENCY)
+    );
   }
 
   @HostListener('focus', ['$event.target.value'])
   onFocus(value: any) {
-    this.element.value = this.formattedCurrencyPipe.parse(value, this.element.getAttribute('locale'), this.element.getAttribute('currency'));
+    this.element.value = this.formattedCurrencyPipe.parse(
+      value,
+      this.element.getAttribute(LOCALE),
+      this.element.getAttribute(CURRENCY)
+    );
   }
 
   @HostListener('blur', ['$event.target.value'])
   onBlur(value: any) {
-    this.element.value = this.formattedCurrencyPipe.transform(value, this.element.getAttribute('locale'), this.element.getAttribute('currency'));
+    this.element.value = this.formattedCurrencyPipe.transform(
+      value,
+      this.element.getAttribute(LOCALE),
+      this.element.getAttribute(CURRENCY)
+    );
+  }
+
+  @HostListener('paste', ['$event'])
+  blockPaste(clipboardEvent: ClipboardEvent, event: Event) {
+    const clipboardFormat = 'text/plain';
+
+    const value = this.formattedCurrencyPipe.parse(
+      clipboardEvent.clipboardData.getData(clipboardFormat),
+      this.element.getAttribute(LOCALE),
+      this.element.getAttribute(CURRENCY)
+    );
+
+    this.element.value = value;
+    clipboardEvent.clipboardData.setData(clipboardFormat, value);
+    clipboardEvent.preventDefault();
+
+    this.ngControl.control.setValue(value);
+  }
+
+  @HostListener('keyup', ['$event'])
+  onKeyUp() {
+    this.element.value = this.formattedCurrencyPipe.parse(
+      this.element.value,
+      this.element.getAttribute(LOCALE),
+      this.element.getAttribute(CURRENCY)
+    );
   }
 }

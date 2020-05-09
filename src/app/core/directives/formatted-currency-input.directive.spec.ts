21b36279660e5234c88715da48e23f4688f94c9f
FurukawaTetsuji <tetsufuru1968@yahoo.co.jp>
2020-05-09 21:12:10 +0900

    😎Added test cases.

--- src/app/core/directives/formatted-currency-input.directive.spec.ts
+++ src/app/core/directives/formatted-currency-input.directive.spec.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, FormsModule, NgControl } from '@angular/forms';
 
 import { FormattedCurrencyPipe } from '../pipes/formatted-currency.pipe';
 import { FormattedCurrencyInputDirective } from './formatted-currency-input.directive';
 
 @Component({
   template: `
-    <input type="text" appFormattedCurrencyInput locale="ja-JP" currency="JPY" />
+    <input type="text" appFormattedCurrencyInput locale="ja-JP" currency="JPY" formControlName="formControl" />
   `
 })
-class TestFormattedCurrencyInputComponent {}
+class TestFormattedCurrencyInputComponent {
+  formControl: FormControl;
+}
+
+const formattedValue = '1,234,567';
+const nonFormattedValue = '1234567';
 
 describe('FormattedCurrencyInputDirective', () => {
   let component: TestFormattedCurrencyInputComponent;
@@ -18,7 +24,7 @@ describe('FormattedCurrencyInputDirective', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FormattedCurrencyPipe],
+      providers: [FormattedCurrencyPipe, FormsModule, NgControl],
       declarations: [TestFormattedCurrencyInputComponent, FormattedCurrencyInputDirective]
     });
     fixture = TestBed.createComponent(TestFormattedCurrencyInputComponent);
@@ -29,21 +35,44 @@ describe('FormattedCurrencyInputDirective', () => {
 
   describe('#onFocus', () => {
     it('should parse', () => {
-      hTMLInputElement.value = '1,234,567';
+      hTMLInputElement.value = formattedValue;
       hTMLInputElement.dispatchEvent(new Event('focus', {}));
       fixture.detectChanges();
 
-      expect(hTMLInputElement.value).toBe('1234567');
+      expect(hTMLInputElement.value).toBe(nonFormattedValue);
     });
   });
 
   describe('#onBlur', () => {
     it('should transfer', () => {
-      hTMLInputElement.value = '1234567';
+      hTMLInputElement.value = nonFormattedValue;
       hTMLInputElement.dispatchEvent(new Event('blur', {}));
       fixture.detectChanges();
 
-      expect(hTMLInputElement.value).toBe('1,234,567');
+      expect(hTMLInputElement.value).toBe(formattedValue);
+    });
+  });
+
+  describe('#beforePaste', () => {
+    it('should transfer', () => {
+      const dataTransfer = new DataTransfer();
+      dataTransfer.setData('text/plain', formattedValue);
+      const clipboardEvent = new ClipboardEvent('paste', { clipboardData: dataTransfer });
+      hTMLInputElement.dispatchEvent(clipboardEvent);
+
+      fixture.whenStable().then(() => {
+        expect(this.formControl.value).toBe(nonFormattedValue);
+      });
+    });
+  });
+
+  describe('#onKeyUp', () => {
+    it('should transfer', () => {
+      hTMLInputElement.value = formattedValue;
+      hTMLInputElement.dispatchEvent(new Event('keyup', {}));
+      fixture.detectChanges();
+
+      expect(hTMLInputElement.value).toBe(nonFormattedValue);
     });
   });
 });
--- src/app/core/directives/formatted-number-input.directive.spec.ts
+++ src/app/core/directives/formatted-number-input.directive.spec.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, FormsModule, NgControl } from '@angular/forms';
 
 import { FormattedNumberPipe } from '../pipes/formatted-number.pipe';
 import { FormattedNumberInputDirective } from './formatted-number-input.directive';
 
 @Component({
-  template: `
-    <input type="text" appFormattedNumberInput locale="ja-JP" />
-  `
+  template: ` <input type="text" appFormattedNumberInput locale="ja-JP" formControlName="formControl" /> `
 })
-class TestFormattedNumberInputComponent {}
+class TestFormattedNumberInputComponent {
+  formControl: FormControl;
+}
+
+const formattedValue = '1,234,567';
+const nonFormattedValue = '1234567';
 
 describe('FormattedNumberInputDirective', () => {
   let component: TestFormattedNumberInputComponent;
@@ -18,7 +22,7 @@ describe('FormattedNumberInputDirective', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FormattedNumberPipe],
+      providers: [FormattedNumberPipe, FormsModule, NgControl],
       declarations: [TestFormattedNumberInputComponent, FormattedNumberInputDirective]
     });
     fixture = TestBed.createComponent(TestFormattedNumberInputComponent);
@@ -44,6 +48,29 @@ describe('FormattedNumberInputDirective', () => {
       fixture.detectChanges();
 
       expect(hTMLInputElement.value).toBe('1,234,567');
+    });
+  });
+
+  describe('#beforePaste', () => {
+    it('should transfer', () => {
+      const dataTransfer = new DataTransfer();
+      dataTransfer.setData('text/plain', formattedValue);
+      const clipboardEvent = new ClipboardEvent('paste', { clipboardData: dataTransfer });
+      hTMLInputElement.dispatchEvent(clipboardEvent);
+
+      fixture.whenStable().then(() => {
+        expect(this.formControl.value).toBe(nonFormattedValue);
+      });
+    });
+  });
+
+  describe('#onKeyUp', () => {
+    it('should transfer', () => {
+      hTMLInputElement.value = formattedValue;
+      hTMLInputElement.dispatchEvent(new Event('keyup', {}));
+      fixture.detectChanges();
+
+      expect(hTMLInputElement.value).toBe(nonFormattedValue);
     });
   });
 });

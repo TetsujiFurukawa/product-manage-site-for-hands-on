6ac88908d9af3513a9f2b018ea3b03867b629067
FurukawaTetsuji <tetsufuru1968@yahoo.co.jp>
2020-05-26 22:30:16 +0900

    😎Reviewed error messaging service's test case.

--- src/app/core/services/error-messaging.service.spec.ts
+++ src/app/core/services/error-messaging.service.spec.ts
@@ -7,9 +7,7 @@ describe('ErrorMessagingService', () => {
   let service: ErrorMessagingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      schemas: [NO_ERRORS_SCHEMA]
-    });
+    TestBed.configureTestingModule({});
     service = TestBed.inject(ErrorMessagingService);
   });
 
@@ -38,57 +36,59 @@ describe('ErrorMessagingService', () => {
   });
 
   describe('#setupPageErrorMessageFromResponse', () => {
-    it('should set property when error status is 400', () => {
-      const error = { status: 400 };
-      service.setupPageErrorMessageFromResponse(error);
-      expect(service.getMessageProperty()).toEqual('errMessage.http.badRequest');
-    });
-    it('should set property when error status is 401', () => {
-      const error = { status: 401 };
-      service.setupPageErrorMessageFromResponse(error);
-      expect(service.getMessageProperty()).toEqual('errMessage.http.unAuthorized');
-    });
-    it('should set property when error status is 402', () => {
-      const error = { status: 404 };
-      service.setupPageErrorMessageFromResponse(error);
-      expect(service.getMessageProperty()).toEqual('errMessage.http.notFound');
-    });
+    const parameters = [
+      {
+        descriptuion: 'should set property when error status is 400',
+        error: { status: 400 },
+        result: 'errMessage.http.badRequest'
+      },
+      {
+        descriptuion: 'should set property when error status is 401',
+        error: { status: 401 },
+        result: 'errMessage.http.unAuthorized'
+      },
+      {
+        descriptuion: 'should set property when error status is 404',
+        error: { status: 404 },
+        result: 'errMessage.http.notFound'
+      },
+      {
+        descriptuion: 'should set property when error status is 500 _ Duplicated key.',
+        error: { status: 500, error: { message: 'Duplicated key.' } },
+        result: 'errMessage.http.duplicateKeyException'
+      },
+      {
+        descriptuion: 'should set property when error status is 500 _ Exclusive error occurred.',
+        error: { status: 500, error: { message: 'Exclusive error occurred.' } },
+        result: 'errMessage.http.exclusiveProcessingException'
+      },
+      {
+        descriptuion: 'should set property when error status is 500 _ There is no stock.',
+        error: { status: 500, error: { message: 'There is no stock.' } },
+        result: 'errMessage.http.outOfStockException'
+      },
+      {
+        descriptuion: 'should set property when error status is 500 _ Data not found.',
+        error: { status: 500, error: { message: 'Data not found.' } },
+        result: 'errMessage.http.datNotFoundException'
+      },
+      {
+        descriptuion: 'should set property when error status is 500 _ Another message',
+        error: { status: 500, error: { message: 'Another message' } },
+        result: 'errMessage.http.internalServerError'
+      },
+      {
+        descriptuion: 'should set property when error status is 501',
+        error: { status: 501 },
+        result: 'errMessage.http.GenericError'
+      }
+    ];
 
-    it('should set property when error status is 500', () => {
-      const error = { status: 500, error: { message: 'Duplicated key.' } };
-      service.setupPageErrorMessageFromResponse(error);
-      expect(service.getMessageProperty()).toEqual('errMessage.http.duplicateKeyException');
-    });
-
-    it('should set property when error status is 500', () => {
-      const error = { status: 500, error: { message: 'Duplicated key.' } };
-      service.setupPageErrorMessageFromResponse(error);
-      expect(service.getMessageProperty()).toEqual('errMessage.http.duplicateKeyException');
-    });
-    it('should set property when error status is 500', () => {
-      const error = { status: 500, error: { message: 'Exclusive error occurred.' } };
-      service.setupPageErrorMessageFromResponse(error);
-      expect(service.getMessageProperty()).toEqual('errMessage.http.exclusiveProcessingException');
-    });
-    it('should set property when error status is 500', () => {
-      const error = { status: 500, error: { message: 'There is no stock.' } };
-      service.setupPageErrorMessageFromResponse(error);
-      expect(service.getMessageProperty()).toEqual('errMessage.http.outOfStockException');
-    });
-    it('should set property when error status is 500', () => {
-      const error = { status: 500, error: { message: 'Data not found.' } };
-      service.setupPageErrorMessageFromResponse(error);
-      expect(service.getMessageProperty()).toEqual('errMessage.http.datNotFoundException');
-    });
-    it('should set property when error status is 500', () => {
-      const error = { status: 500, error: { message: 'abcde' } };
-      service.setupPageErrorMessageFromResponse(error);
-      expect(service.getMessageProperty()).toEqual('errMessage.http.internalServerError');
-    });
-    it('should set property when error status is 501', () => {
-      const error = { status: 501 };
-      service.setupPageErrorMessageFromResponse(error);
-      expect(service.getMessageProperty()).toEqual('errMessage.http.GenericError');
+    parameters.forEach((parameter) => {
+      it(parameter.descriptuion, () => {
+        service.setupPageErrorMessageFromResponse(parameter.error);
+        expect(service.getMessageProperty()).toEqual(parameter.result);
+      });
     });
   });
 });

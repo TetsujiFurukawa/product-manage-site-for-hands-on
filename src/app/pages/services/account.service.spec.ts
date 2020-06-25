01dcb04e0b8951d589f3b80ada009fd8662560fe
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2020-06-25 22:49:47 +0900

    😎Reviewed test case.

--- src/app/pages/services/account.service.spec.ts
+++ src/app/pages/services/account.service.spec.ts
@@ -5,6 +5,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { ErrorMessagingService } from '../../core/services/error-messaging.service';
 import { ApiConst } from '../constants/api-const';
+import { SignInRequestDto } from '../models/dtos/requests/sign-in-request-dto';
 import { MenuListResponseDto } from '../models/dtos/responses/menu-list-response-dto';
 import { SignInResponseDto } from '../models/dtos/responses/sign-in-response-dto';
 import { User } from '../models/user';
@@ -40,10 +41,11 @@ describe('AccountService', () => {
     const webApiUrl = ApiConst.PATH_API_ROOT + ApiConst.PATH_SIGN_IN;
 
     it('should return expected response', () => {
+      const signInRequestDto: SignInRequestDto = createSignInRequestDto();
       const expectedSignInResponseDto: SignInResponseDto = createExpectedSignInResponseDto();
 
       // Subscribes to api.
-      service.signIn({ Username: '', Password: '' }).subscribe((signInResponseDto) => {
+      service.signIn(signInRequestDto).subscribe((signInResponseDto) => {
         expect(signInResponseDto).toEqual(expectedSignInResponseDto);
         expect(errorMessagingServiceSpy.setupPageErrorMessageFromResponse.calls.count()).toBe(0);
       }, fail);
@@ -55,11 +57,12 @@ describe('AccountService', () => {
     });
 
     it('should return null when response is 401 Unauthorized error', () => {
+      const signInRequestDto: SignInRequestDto = createSignInRequestDto();
       const errorStatus = 401;
       const errorMessage = '401 Unauthorized';
 
       // Subscribes to api.
-      service.signIn({ Username: '', Password: '' }).subscribe((signInResponseDto) => {
+      service.signIn(signInRequestDto).subscribe((signInResponseDto) => {
         expect(signInResponseDto).toBeNull();
         expect(errorMessagingServiceSpy.setupPageErrorMessageFromResponse.calls.count()).toBe(1);
       }, fail);
@@ -165,7 +168,7 @@ describe('AccountService', () => {
     it('should return expected data', () => {
       const user: User = createExpectedUser();
       service.setUser(user);
-      expectUser(service, service.getUser());
+      assertUser(service, service.getUser());
     });
   });
 
@@ -173,7 +176,7 @@ describe('AccountService', () => {
     it('should remove user', () => {
       const user: User = createExpectedUser();
       service.setUser(user);
-      expectUser(service, service.getUser());
+      assertUser(service, service.getUser());
 
       service.removeUser();
       expect(service.getUser()).toBeNull();
@@ -181,14 +184,18 @@ describe('AccountService', () => {
   });
 });
 
+function createSignInRequestDto() {
+  return { Username: 'Username', Password: 'Password' };
+}
+
 function createExpectedSignInResponseDto() {
   return {
     userAccount: 'userAccount',
     userName: 'userName',
     userLocale: 'ja-JP',
     userLanguage: 'ja',
-    userTimezone: 'UTC',
-    userTimezoneOffset: '0000',
+    userTimezone: 'Asia/Tokyo',
+    userTimezoneOffset: '+0900',
     userCurrency: 'JPY'
   };
 }
@@ -206,19 +213,21 @@ function createExpectedMenuListResponseDto() {
 function createExpectedUser() {
   const user: User = new User();
   user.userAccount = 'userAccount';
-  user.userCurrency = 'JPY';
-  user.userLanguage = 'ja';
-  user.userLocale = 'js-JP';
   user.userName = 'userName';
-  user.userTimezone = 'UTC';
+  user.userLocale = 'ja-JP';
+  user.userLanguage = 'ja';
+  user.userTimezone = 'Asia/Tokyo';
+  user.userTimezoneOffset = '+0900';
+  user.userCurrency = 'JPY';
   return user;
 }
 
-function expectUser(accountService: AccountService, user: User) {
+function assertUser(accountService: AccountService, user: User) {
   expect(accountService.getUser().userAccount).toEqual(user.userAccount);
-  expect(accountService.getUser().userCurrency).toEqual(user.userCurrency);
-  expect(accountService.getUser().userLanguage).toEqual(user.userLanguage);
-  expect(accountService.getUser().userLocale).toEqual(user.userLocale);
   expect(accountService.getUser().userName).toEqual(user.userName);
+  expect(accountService.getUser().userLocale).toEqual(user.userLocale);
+  expect(accountService.getUser().userLanguage).toEqual(user.userLanguage);
   expect(accountService.getUser().userTimezone).toEqual(user.userTimezone);
+  expect(accountService.getUser().userTimezoneOffset).toEqual(user.userTimezoneOffset);
+  expect(accountService.getUser().userCurrency).toEqual(user.userCurrency);
 }

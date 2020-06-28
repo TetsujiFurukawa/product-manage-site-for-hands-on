1e8ba91d68f3248ea71032e0a6fb87e624e82cc0
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2020-06-29 01:24:55 +0900

    😎Reviewed test case.

--- src/app/pages/components/sign-in-page/sign-in-page.component.spec.ts
+++ src/app/pages/components/sign-in-page/sign-in-page.component.spec.ts
@@ -1,13 +1,10 @@
 import { TranslateTestingModule } from 'ngx-translate-testing';
 import { of } from 'rxjs';
-import { MaterialModule } from 'src/app/material/material.module';
 import { UrlConst } from 'src/app/pages/constants/url-const';
-import { User } from 'src/app/pages/models/user';
 import { AccountService } from 'src/app/pages/services/account.service';
 import { TitleI18Service } from 'src/app/shared/services/title-i18.service';
 import { HtmlElementUtility } from 'src/app/tetsing/html-element-utility';
 
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
@@ -21,24 +18,8 @@ import { SignInResponseDto } from '../../models/dtos/responses/sign-in-response-
 import { SignInPageComponent } from './sign-in-page.component';
 
 describe('SignInPageComponent', () => {
-  const user: User = new User();
-  user.userAccount = 'userAccount';
-  user.userCurrency = 'JPY';
-  user.userLanguage = 'ja';
-  user.userLocale = 'ja-JP';
-  user.userName = 'userName';
-  user.userTimezone = 'UTC';
-
-  const expectedRequest: SignInRequestDto = { Password: 'Password', Username: 'Username' };
-
-  const expectedResponse = {
-    userAccount: 'userAccount',
-    userCurrency: 'JPY',
-    userLanguage: 'ja',
-    userLocale: 'ja-JP',
-    userName: 'userName',
-    userTimezone: ''
-  };
+  const expectedSignInRequestDto: SignInRequestDto = createExpectedRequestDto();
+  const expectedResponse: SignInResponseDto = createExpectedResponseDto();
 
   let component: SignInPageComponent;
   let fixture: ComponentFixture<SignInPageComponent>;
@@ -53,10 +34,8 @@ describe('SignInPageComponent', () => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       imports: [
-        HttpClientTestingModule,
         RouterTestingModule,
         TranslateTestingModule.withTranslations({ ja: require('src/assets/i18n/ja.json') }),
-        MaterialModule,
         BrowserAnimationsModule,
         ReactiveFormsModule
       ],
@@ -74,7 +53,7 @@ describe('SignInPageComponent', () => {
     fixture = TestBed.createComponent(SignInPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    setupNavigatorLanguage('ja');
+    setupBrowserLanguage('ja');
   });
 
   describe('#constractor', () => {
@@ -82,7 +61,7 @@ describe('SignInPageComponent', () => {
       expect(component).toBeTruthy();
     });
     it('should create when navigator.langage returns ja-JP', () => {
-      setupNavigatorLanguage('ja-jp');
+      setupBrowserLanguage('ja-jp');
       expect(component).toBeTruthy();
     });
   });
@@ -122,12 +101,12 @@ describe('SignInPageComponent', () => {
       expect(htmlInputElement.innerText).toContain('EXAPMLE SITE');
     });
 
-    it('signin user account', () => {
+    it('sign in user account', () => {
       const htmlInputElement: HTMLInputElement = fixture.debugElement.query(By.css('#signin-user-account'))
         .nativeElement;
       expect(htmlInputElement.placeholder).toContain('ユーザアカウント');
     });
-    it('signin user password', () => {
+    it('sign in user password', () => {
       const htmlInputElement: HTMLInputElement = fixture.debugElement.query(By.css('#signin-user-password'))
         .nativeElement;
       expect(htmlInputElement.placeholder).toContain('パスワード');
@@ -139,12 +118,12 @@ describe('SignInPageComponent', () => {
   });
 
   describe('DOM input test', () => {
-    it('signin user account', () => {
+    it('sign in user account', () => {
       const expectedValue = expectedResponse.userAccount;
       HtmlElementUtility.setValueToHTMLInputElement(fixture, '#signin-user-account', expectedValue);
       expect(component.signInUserAccount.value).toEqual(expectedValue);
     });
-    it('signin user password', () => {
+    it('sign in user password', () => {
       const expectedValue = 'productName';
       HtmlElementUtility.setValueToHTMLInputElement(fixture, '#signin-user-password', expectedValue);
       expect(component.signInUserPassword.value).toEqual(expectedValue);
@@ -152,12 +131,12 @@ describe('SignInPageComponent', () => {
   });
 
   describe('DOM input validation test', () => {
-    it('signin user account', () => {
+    it('sign in user account', () => {
       HtmlElementUtility.setValueToHTMLInputElement(fixture, '#signin-user-account', '');
       const validationError = fixture.nativeElement.querySelector('.validation-error');
       expect(validationError).toBeTruthy();
     });
-    it('signin user password', () => {
+    it('sign in user password', () => {
       HtmlElementUtility.setValueToHTMLInputElement(fixture, '#signin-user-password', '');
       const validationError = fixture.nativeElement.querySelector('.validation-error');
       expect(validationError).toBeTruthy();
@@ -166,19 +145,39 @@ describe('SignInPageComponent', () => {
 
   describe('DOM input test', () => {
     it('Should Enter input and create request', () => {
-      HtmlElementUtility.setValueToHTMLInputElement(fixture, '#signin-user-account', expectedRequest.Username);
-      HtmlElementUtility.setValueToHTMLInputElement(fixture, '#signin-user-password', expectedRequest.Password);
+      HtmlElementUtility.setValueToHTMLInputElement(fixture, '#signin-user-account', expectedSignInRequestDto.Username);
+      HtmlElementUtility.setValueToHTMLInputElement(
+        fixture,
+        '#signin-user-password',
+        expectedSignInRequestDto.Password
+      );
 
       const targetMethodName = 'createSignInRequestDto';
       const signInRequestDto: SignInRequestDto = component[targetMethodName]();
 
-      expect(signInRequestDto.Username).toEqual(expectedRequest.Username);
-      expect(signInRequestDto.Password).toEqual(expectedRequest.Password);
+      expect(signInRequestDto.Username).toEqual(expectedSignInRequestDto.Username);
+      expect(signInRequestDto.Password).toEqual(expectedSignInRequestDto.Password);
     });
   });
 });
 
-function setupNavigatorLanguage(language: string) {
+function createExpectedRequestDto(): SignInRequestDto {
+  return { Password: 'Password', Username: 'Username' };
+}
+
+function createExpectedResponseDto(): SignInResponseDto {
+  return {
+    userAccount: 'userAccount',
+    userName: 'userName',
+    userLocale: 'ja-JP',
+    userLanguage: 'ja',
+    userTimezone: 'Asia/Tokyo',
+    userTimezoneOffset: '+0900',
+    userCurrency: 'JPY'
+  };
+}
+
+function setupBrowserLanguage(language: string) {
   const defineGetter = '__defineGetter__';
   window.navigator[defineGetter]('language', () => {
     return language;

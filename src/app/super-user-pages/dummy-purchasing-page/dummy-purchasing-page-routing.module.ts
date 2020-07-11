c0fdec32dd2f9d9526cb9f663f90d642a7140952
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2020-07-12 00:47:39 +0900

    😎moved auth guard from super-user-page router to app router.

--- src/app/super-user-pages/dummy-purchasing-page/dummy-purchasing-page-routing.module.ts
+++ src/app/super-user-pages/dummy-purchasing-page/dummy-purchasing-page-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DummyPurchasingPageComponent } from './dummy-purchasing-page.component';
 
-const routes: Routes = [{ path: '', component: DummyPurchasingPageComponent, canActivate: [AuthGuard] }];
+const routes: Routes = [{ path: '', component: DummyPurchasingPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

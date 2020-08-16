92f5437ec055a0a73158fa85de43566d72b00d46
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2020-08-16 20:33:09 +0900

    😎Deleted unnecessary services from shared.module.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,9 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatPaginatorIntl } from '@angular/material/paginator';
 import { RouterModule } from '@angular/router';
 
 import { CoreModule } from '../core/core.module';
-import { MatPaginatorI18nService } from '../core/services/mat-paginator-i18n.service';
 import { MaterialModule } from '../material/material.module';
 import { NgxTranslateModule } from '../ngx-translate/ngx-translate.module';
 import { FooterComponent } from './components/footer/footer.component';
@@ -27,7 +25,6 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
     CoreModule,
     NgxTranslateModule
   ],
-  providers: [{ provide: MatPaginatorIntl, useClass: MatPaginatorI18nService }],
   exports: [HeaderComponent, FooterComponent, SidenavComponent]
 })
 export class SharedModule {}

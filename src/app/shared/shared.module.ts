a534bffcbb0c86f4a5648186b097f3758518384b
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2020-08-22 09:33:18 +0900

    😎Removed HttpClientModule form shared.module.ts.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,5 @@
-import { NgxUpperCaseDirectiveModule } from 'ngx-upper-case-directive';
-
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { CoreModule } from '../core/core.module';

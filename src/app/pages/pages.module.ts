f0e8c83bb30ae4f17ce68b0bba15eb37641b114e
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2021-01-07 01:03:26 +0900

    ✨Reviewed and fixed directives.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,23 +8,17 @@ import { MatPaginatorIntl } from '@angular/material/paginator';
 import { RouterModule } from '@angular/router';
 
 import { CoreModule } from '../core/core.module';
+import { FormattedCurrencyPipe } from '../core/pipes/formatted-currency.pipe';
+import { FormattedNumberPipe } from '../core/pipes/formatted-number.pipe';
 import { MatPaginatorI18nService } from '../core/services/mat-paginator-i18n.service';
 import { MaterialModule } from '../material/material.module';
 import { NgxTranslateModule } from '../ngx-translate/ngx-translate.module';
 import { SharedModule } from '../shared/shared.module';
-import {
-    ProductListingPageComponent
-} from './components/product-listing-page/product-listing-page.component';
-import {
-    ProductRegisteringPageComponent
-} from './components/product-registering-page/product-registering-page.component';
-import {
-    PurchaseHistoryListingPageComponent
-} from './components/purchase-history-listing-page/purchase-history-listing-page.component';
+import { ProductListingPageComponent } from './components/product-listing-page/product-listing-page.component';
+import { ProductRegisteringPageComponent } from './components/product-registering-page/product-registering-page.component';
+import { PurchaseHistoryListingPageComponent } from './components/purchase-history-listing-page/purchase-history-listing-page.component';
 import { SignInPageComponent } from './components/sign-in-page/sign-in-page.component';
-import {
-    StockRegisteringPageComponent
-} from './components/stock-registering-page/stock-registering-page.component';
+import { StockRegisteringPageComponent } from './components/stock-registering-page/stock-registering-page.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +39,11 @@ import {
     SharedModule,
     NgxTranslateModule
   ],
-  providers: [{ provide: MatPaginatorIntl, useClass: MatPaginatorI18nService }],
+  providers: [
+    { provide: MatPaginatorIntl, useClass: MatPaginatorI18nService },
+    FormattedCurrencyPipe,
+    FormattedNumberPipe
+  ],
   exports: [
     ProductListingPageComponent,
     ProductRegisteringPageComponent,

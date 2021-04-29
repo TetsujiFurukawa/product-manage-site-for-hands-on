805a0e8e43baf2d0e2a57782f42614fd7476827b
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2021-04-29 10:36:27 +0900

    ✨Changed test of purchase quantity stock quantity validator to parameterized test.

--- src/app/pages/validators/purchase-quantity-stock-quantity-validator.spec.ts
+++ src/app/pages/validators/purchase-quantity-stock-quantity-validator.spec.ts
@@ -15,52 +15,62 @@ describe('PurchaseQuantityStockQuantityValidator', () => {
   });
 
   describe('#validate', () => {
-    it('should not have error | productStockQuantity is blank', () => {
-      testingForm.setValue({ productStockQuantity: '', productPurchaseQuantity: 1 });
-      const validatorFn = PurchaseQuantityStockQuantityValidator(LOCALE);
-      validatorFn(testingForm);
-      expect(testingForm.get(PRODUCT_PURCHASE_QUANTITY).getError('exceedStockError')).toBeNull();
-    });
-    it('should not have error | productStockQuantity is null', () => {
-      testingForm.setValue({ productStockQuantity: null, productPurchaseQuantity: 1 });
-      const validatorFn = PurchaseQuantityStockQuantityValidator(LOCALE);
-      validatorFn(testingForm);
-      expect(testingForm.get(PRODUCT_PURCHASE_QUANTITY).getError('exceedStockError')).toBeNull();
-    });
-
-    it('should not have error | productPurchaseQuantity is blank', () => {
-      testingForm.setValue({ productStockQuantity: 1, productPurchaseQuantity: '' });
-      const validatorFn = PurchaseQuantityStockQuantityValidator(LOCALE);
-      validatorFn(testingForm);
-      expect(testingForm.get(PRODUCT_PURCHASE_QUANTITY).getError('exceedStockError')).toBeNull();
-    });
-
-    it('should not have error | productPurchaseQuantity is null', () => {
-      testingForm.setValue({ productStockQuantity: 1, productPurchaseQuantity: null });
-      const validatorFn = PurchaseQuantityStockQuantityValidator(LOCALE);
-      validatorFn(testingForm);
-      expect(testingForm.get(PRODUCT_PURCHASE_QUANTITY).getError('exceedStockError')).toBeNull();
-    });
-
-    it('should not have error | productPurchaseQuantity is not numeric', () => {
-      testingForm.setValue({ productStockQuantity: 1, productPurchaseQuantity: 'a' });
-      const validatorFn = PurchaseQuantityStockQuantityValidator(LOCALE);
-      validatorFn(testingForm);
-      expect(testingForm.get(PRODUCT_PURCHASE_QUANTITY).getError('exceedStockError')).toBeNull();
-    });
-
-    it('should not have error | productPurchaseQuantity equals productStockQuantity', () => {
-      testingForm.setValue({ productStockQuantity: 1, productPurchaseQuantity: 1 });
-      const validatorFn = PurchaseQuantityStockQuantityValidator(LOCALE);
-      validatorFn(testingForm);
-      expect(testingForm.get(PRODUCT_PURCHASE_QUANTITY).getError('exceedStockError')).toBeNull();
-    });
-
-    it('should have error | productPurchaseQuantity exceeds productStockQuantity', () => {
-      testingForm.setValue({ productStockQuantity: 1, productPurchaseQuantity: 2 });
-      const validatorFn = PurchaseQuantityStockQuantityValidator(LOCALE);
-      validatorFn(testingForm);
-      expect(testingForm.get(PRODUCT_PURCHASE_QUANTITY).getError('exceedStockError')).toBeTruthy();
+    const parameters = [
+      {
+        description: 'should not have error | productStockQuantity is blank',
+        productStockQuantity: '',
+        productPurchaseQuantity: 1,
+        expectedErrorOfProductPurchaseQuantity: null
+      },
+      {
+        description: 'should not have error | productStockQuantity is null',
+        productStockQuantity: null,
+        productPurchaseQuantity: 1,
+        expectedErrorOfProductPurchaseQuantity: null
+      },
+      {
+        description: 'should not have error | productPurchaseQuantity is blank',
+        productStockQuantity: 1,
+        productPurchaseQuantity: '',
+        expectedErrorOfProductPurchaseQuantity: null
+      },
+      {
+        description: 'should not have error | productPurchaseQuantity is null',
+        productStockQuantity: 1,
+        productPurchaseQuantity: null,
+        expectedErrorOfProductPurchaseQuantity: null
+      },
+      {
+        description: 'should not have error | productPurchaseQuantity is not numeric',
+        productStockQuantity: 1,
+        productPurchaseQuantity: 'a',
+        expectedErrorOfProductPurchaseQuantity: null
+      },
+      {
+        description: 'should not have error | productPurchaseQuantity equals productStockQuantity',
+        productStockQuantity: 1,
+        productPurchaseQuantity: 1,
+        expectedErrorOfProductPurchaseQuantity: null
+      },
+      {
+        description: 'should have error | productPurchaseQuantity exceeds productStockQuantity',
+        productStockQuantity: 1,
+        productPurchaseQuantity: 2,
+        expectedErrorOfProductPurchaseQuantity: true
+      }
+    ];
+    parameters.forEach((parameter) => {
+      it(parameter.description, () => {
+        testingForm.setValue({
+          productStockQuantity: parameter.productStockQuantity,
+          productPurchaseQuantity: parameter.productPurchaseQuantity
+        });
+        const validatorFn = PurchaseQuantityStockQuantityValidator(LOCALE);
+        validatorFn(testingForm);
+        expect(testingForm.get(PRODUCT_PURCHASE_QUANTITY).getError('exceedStockError')).toEqual(
+          parameter.expectedErrorOfProductPurchaseQuantity
+        );
+      });
     });
   });
 });

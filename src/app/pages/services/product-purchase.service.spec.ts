d577009bf3a9372d58ac984fdd7929a87a9f5d28
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2021-05-15 20:21:16 +0900

    ✨Reviewed and fixed product-purchase.service.spec.ts.

--- src/app/pages/services/product-purchase.service.spec.ts
+++ src/app/pages/services/product-purchase.service.spec.ts
@@ -1,6 +1,5 @@
 import { HttpParams } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 import { ErrorMessagingService } from '../../core/services/error-messaging.service';
@@ -36,7 +35,6 @@ describe('ProductPurchaseService', () => {
       'setupPageErrorMessageFromResponse'
     ]);
     TestBed.configureTestingModule({
-      schemas: [NO_ERRORS_SCHEMA],
       imports: [HttpClientTestingModule],
       providers: [
         { provide: SuccessMessagingService, useValue: successMessagingServiceSpy },
